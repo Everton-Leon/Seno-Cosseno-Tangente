2838864f778b57471e40db47b48889a9902c4875
Everton-Leon <evertonleon07@gmail.com>
2022-12-26 21:34:16 -0300

    commit inicial

---
+++ ex017.py
@@ -0,0 +1,9 @@
+# importando biblioteca
+import math
+a = float(input('Digite o ângulo: '))
+
+# convertendo o valor em rediando
+xr = math.radians(a)
+
+# reposta
+print(f'O seno de {a} é {math.sin(xr):.2f}\nO cosseno de {a} é {math.cos(xr):.2f}\nA tangente de {a} é {math.tan(xr):.2f}')
